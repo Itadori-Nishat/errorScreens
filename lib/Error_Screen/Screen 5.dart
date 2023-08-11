0d09b94afa1e45abb4198217cd1f50fce0e1258f
Itadori-Nishat <104446439+Itadori-Nishat@users.noreply.github.com>
2023-08-12 01:52:28 +0600

    multiple image added. show image count. tap to zoom in/out.

--- lib/Error_Screen/Screen 5.dart
+++ lib/Error_Screen/Screen 5.dart
@@ -1,40 +1,10 @@
 import 'package:flutter/material.dart';
 
-
 class ScreenFive extends StatelessWidget {
-  final List<String> imageUrls = [
-    'https://plus.unsplash.com/premium_photo-1681245141314-fb0b76bf164e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-    'https://images.unsplash.com/photo-1691676112160-53cddd988388?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-    'https://plus.unsplash.com/premium_photo-1681245141314-fb0b76bf164e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-    'https://images.unsplash.com/photo-1691676112160-53cddd988388?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-    'https://plus.unsplash.com/premium_photo-1681245141314-fb0b76bf164e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-    'https://images.unsplash.com/photo-1691676112160-53cddd988388?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-    'https://plus.unsplash.com/premium_photo-1681245141314-fb0b76bf164e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-    'https://images.unsplash.com/photo-1691676112160-53cddd988388?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-
-  ];
+  const ScreenFive({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Grid'),
-      ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3, // Number of columns in the grid
-          crossAxisSpacing: 10, // Spacing between columns
-          mainAxisSpacing: 10, // Spacing between rows
-        ),
-        itemCount: imageUrls.length,
-        itemBuilder: (context, index) {
-          return Image.network(
-            imageUrls[index],
-            fit: BoxFit.cover,
-          );
-        },
-      ),
-    );
+    return Scaffold();
   }
 }
